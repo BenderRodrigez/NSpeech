562133948929ce5a6c8c6727cec52c63ce0062ec
BenderRodrigez <mr.makoto.kun@gmail.com>
2016-07-17 19:11:16 +0400

    Add SpeechSignal class

---
+++ NSpeech/NSpeech/SpeechSignal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NSpeech
+{
+    class SpeechSignal:Signal
+    {
+        public SpeechSignal(float[] samples, int sampleRate) : base(samples, sampleRate)
+        {
+        }
+
+        public List<Tuple<int, int>> GetVoicedSpeechIntervals()
+        {
+            return null;//TODO: implemet this
+        }
+    }
+}
